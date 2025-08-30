c8724d90edddad3b0e7ebcf29d4734dc0c693e3f
krishna sain <sainkrishna510@gmail.com>
2025-08-30 12:33:14 +0530

    Initial commit: Zerodha Clone (MERN)

--- Daeshboard/src/components/BuyActionWindow.jsx
+++ Daeshboard/src/components/BuyActionWindow.jsx
@@ -12,7 +12,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 const closeBuyWindow=useContext(GeneralContext);
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://zerodha-backend-uy7a.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
